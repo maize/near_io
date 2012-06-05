724328282e6b6c42d3f655454b3d9f54da7717ab
Matthias Link <info@mlink.co.uk>
2012-06-06 00:02:47 +0100

    delete more files

--- app/models/locations_note.rb
+++
@@ -1,5 +0,0 @@
-class LocationsNote < ActiveRecord::Base
-  attr_accessible :location_id, :note_id
-  belongs_to :location
-  belongs_to :note
-end
--- test/unit/allocation_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AllocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
