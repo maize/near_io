c60d3f322dc439743bc5040e728bd62934221cb5
Will Hines <w@williamehines.com>
2012-06-17 00:52:15 +0100

    Crawler implementation

--- lib/apis/crawler.rb
+++ lib/apis/crawler.rb
@@ -1,6 +1,12 @@
+=begin
+Crawls websites, stores to MongoDB based.
+=end
+
 require 'anemone'  
 require 'open-uri'  
 require 'mongo'
+
+include Mongoid::Timestamps # adds automagic fields created_at, updated_at
 
 class Apis::Crawler
 
@@ -13,33 +19,39 @@ def spider(url, place)
      :retry_limit => 0,
      :verbose => true,
      :discard_page_bodies => true,
-     :user_agent => 'Mozilla...'
+     :obey_robots_txt => true
+     :depth_limit => 3
    }
 
-  Anemone.crawl(url, options) do |anemone|
-      anemone.storage = Anemone::Storage.MongoDB
-      puts "crawling #{url}"    
-      anemone.on_every_page do |page|
-        if( place =~ page.css("title") ) 
-          puts page.css("title").text   
-          puts page.url
-            news = Newsitem.new(
-            {
-            :title => title,
-            :url => url,
-            :siteId => siteId
-            }
-          )
-        end
-      end
-  end 
-end
+  begin
+      Anemone.crawl(url, options) do |anemone|
+          anemone.storage = Anemone::Storage.MongoDB
+          puts "crawling #{url}"    
+          anemone.on_every_page do |page|
+                    if page.doc
+                      title = page.doc.at('title').text
+                      meta_desc = page.doc.css("meta[name='description']").first 
+                      content = meta_desc['content'] 
 
-def spider_list(list, duration=)
+                        if((content =~ /#{tag}/) or (title =~ /#{tag}/))
+                          news = Newsitem.new(
+                          {
+                          :title => page.css("title").text   
+                          :url => page.url
+                          :siteId => source
+                          }
+                        else
+                            puts "No match for #{place} indexed." 
+                        end     
+                    end
+            end
+          end
+      end 
+    end
+  
 
-  @array.each do |item|
-    item.spider(self, place)
+  def spider_list(list, place)
+    list.each do |page|
+      item.spider(page, place)
+    end
   end
-end
-
-
