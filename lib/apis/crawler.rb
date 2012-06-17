3e7a3aa45434160d86f5dd9b5ba6084d15349cc6
Will Hines <w@williamehines.com>
2012-06-17 01:00:39 +0100

    Crawler cleanup

--- lib/apis/crawler.rb
+++ lib/apis/crawler.rb
@@ -10,48 +10,49 @@ include Mongoid::Timestamps # adds automagic fields created_at, updated_at
 
 class Apis::Crawler
 
-def spider(url, place)
+  def spider(url, place)
 
-  options = { 
-     :duration => 5,
-     :accept_cookies => true,
-     :read_timeout => 20,
-     :retry_limit => 0,
-     :verbose => true,
-     :discard_page_bodies => true,
-     :obey_robots_txt => true
-     :depth_limit => 3
-   }
+    options = { 
+       :duration => 5,
+       :accept_cookies => true,
+       :read_timeout => 20,
+       :retry_limit => 0,
+       :verbose => true,
+       :discard_page_bodies => true,
+       :obey_robots_txt => true
+       :depth_limit => 3
+     }
 
-  begin
-      Anemone.crawl(url, options) do |anemone|
-          anemone.storage = Anemone::Storage.MongoDB
-          puts "crawling #{url}"    
-          anemone.on_every_page do |page|
-                    if page.doc
-                      title = page.doc.at('title').text
-                      meta_desc = page.doc.css("meta[name='description']").first 
-                      content = meta_desc['content'] 
+    begin
+        Anemone.crawl(url, options) do |anemone|
+            anemone.storage = Anemone::Storage.MongoDB
+            puts "crawling #{url}"    
+              anemone.on_every_page do |page|
+                      if page.doc
+                        title = page.doc.at('title').text
+                        meta_desc = page.doc.css("meta[name='description']").first 
+                        content = meta_desc['content'] 
 
-                        if((content =~ /#{tag}/) or (title =~ /#{tag}/))
-                          news = Newsitem.new(
-                          {
-                          :title => page.css("title").text   
-                          :url => page.url
-                          :siteId => source
-                          }
-                        else
-                            puts "No match for #{place} indexed." 
-                        end     
-                    end
-            end
-          end
-      end 
-    end
-  
-
-  def spider_list(list, place)
-    list.each do |page|
-      item.spider(page, place)
-    end
+                          if((content =~ /#{place}/) or (title =~ /#{place}/))
+                            news = Newsitem.new(
+                            {
+                            :title => title   
+                            :url => page.url
+                            :siteId => source
+                            }
+                          else
+                              puts "No match for #{place} indexed." 
+                          end     
+                      end
+              end
+        end
+    end 
   end
+
+    def spider_list(list, place)
+      list.each do |page|
+        item.spider(page, place)
+      end
+    end
+
+end
