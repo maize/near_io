5aae719eb178f7835a6d20adeb154cc9385607be
Matthias Link <info@mlink.co.uk>
2012-08-02 16:55:23 +0100

    added method missing to eventful api

--- lib/apis/eventful_api.rb
+++ lib/apis/eventful_api.rb
@@ -15,4 +15,17 @@ class Apis::EventfulApi
   def self.get_events_nearby(lat, lon, radius)
     get("/events/search?location='#{lat},#{lon}'&within=#{radius}")
   end
+
+  def self.method_missing(method_id, *args)
+    if match = /get_events_([_a-zA-Z]\w*)/.match(method_id.to_s)
+      attribute_names = match.captures.last.split('_and_')
+ 
+      request = ""
+      attribute_names.each_with_index { |name, idx| request = request + name + "=" + args[idx] + (attribute_names.length-1 == idx ? "" : "&") }
+ 
+      get_products(request)
+    else
+      super
+    end
+  end
 end
