5cb5df3eb371e47a8fed2cd7b22346a93e553e50
Matthias Link <info@mlink.co.uk>
2012-08-06 14:16:05 +0100

    compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ NnApi::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( application.js application.js )
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
