c635ecee01d72e15dc670a0e6e6e64ef30832297
Matthias Link <info@mlink.co.uk>
2012-08-06 14:22:12 +0100

    fooling around with precompilation of assets

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module NnApi
@@ -61,8 +61,6 @@ module NnApi
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.assets.initialize_on_precompile = false
 
     config.assets.logger = false
     config.assets.logger = nil
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,7 @@ NnApi::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.assets.initialize_on_precompile = true
 
   # Do not compress assets
   config.assets.compress = false
