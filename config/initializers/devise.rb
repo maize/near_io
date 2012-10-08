c238993e35ccf1e42c325cfc474027c67117a574
maize <info@mlink.co.uk>
2012-10-08 01:40:55 -0700

    redirecting sign up users to network/ucl

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "info@nearnote.co.uk"
+  config.mailer_sender = "info@near.io"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
