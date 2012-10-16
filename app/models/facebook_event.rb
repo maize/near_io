0d02ef8fe88cca41b4569fe93a889f1c86724591
maize <info@mlink.co.uk>
2012-10-16 12:40:07 -0700

    fixing date

--- app/models/facebook_event.rb
+++ app/models/facebook_event.rb
@@ -77,7 +77,7 @@ class FacebookEvent
 
     p Time.zone.to_s
     p Time.zone.now.to_s
-    p "Parsed to UTC: "+Time.zone.parse(parsed).utc
+    p "Parsed to UTC: "+Time.zone.parse(parsed).utc.to_s
   end
 
   def self.parse_event_users(hash)
